3159f6a93e0ed78feb5bb76ea7ecc813fe62edaa
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-06-22 19:50:31 +0200

    removed demo data

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,10 @@ const Home = async () => {
     .from("articles")
     .select("*");
 
-  const demoCards = [];
-
-  for (let i = 0; i < 11; i++) {
-    demoCards.push(articles?.[0]);
-  }
-
   return (
     <div className="max-w-[85rem] h-full px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <div className="grid max-sm:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-24">
-        {demoCards?.map((post) => (
+        {articles?.map((post) => (
           <Card post={post} key={post?.id} />
         ))}
       </div>
