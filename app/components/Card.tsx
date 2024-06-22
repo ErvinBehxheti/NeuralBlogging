25aa89db2088b5b324ac5d8d9b1a066093a49885
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-06-22 15:53:30 +0200

    connected to supabase

---
+++ app/components/Card.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export type Post = {
+  post: {
+    id: number;
+    author: string;
+    image: string;
+    title: string;
+    content: string;
+  };
+};
+
+const Card = ({ post }: Post) => {
+  return (
+    <article className="rounded-xl overflow-hidden border border-white border-opacity-0 hover:border-opacity-100">
+      <Link
+        href={`/${post.id}`}
+        className="block bg-white/20 backdrop-blur-md shadow p-4"
+      >
+        <div className="relative block h-96 w-full">
+          <Image
+            className="rounded-lg"
+            src={`http://res.cloudinary.com/diaxmj0pa/image/fetch/w_auto,f_auto/https://opplwblqtuvbutcbnlbg.supabase.co/storage/v1/object/public/images/${post.image}`}
+            alt={post.title}
+            priority
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className="p-2 pt-6 newsreader h-auto text-white">
+          <h5 className="mb-2 text-xl font-medium leading-tight">
+            {post.title}
+          </h5>
+          <p className="mb-4 text-base truncate">{post.content}</p>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default Card;
